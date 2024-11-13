d5b01e7bef8518d3789662fb4f66baf0cd450c57
magiobus <magio@magiobus.com>
2024-11-13 14:33:55 -0600

    basic config

---
+++ config.js
@@ -0,0 +1,13 @@
+import themes from "daisyui/src/theming/themes";
+
+const config = {
+  // REQUIRED
+  appName: "Escuelita Starter",
+  // REQUIRED: a short description of your app for SEO tags (can be overwritten)
+  appDescription:
+    "Escuelita Starter is a template to start creating your own projects FAST",
+  // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
+  domainName: "escuelitamaker.com",
+};
+
+export default config;
