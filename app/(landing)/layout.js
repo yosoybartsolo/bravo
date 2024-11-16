d77b2bb86a7132b2620442115141db9156b79ef9
magiobus <magio@magiobus.com>
2024-11-15 19:24:29 -0700

    empty layout

--- app/(landing)/layout.js
+++ app/(landing)/layout.js
@@ -1,6 +1,3 @@
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
-
 export default function LayoutLanding({ children }) {
   return <>{children}</>;
 }
