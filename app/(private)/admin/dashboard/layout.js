4a0e6314790fc145fe4ee40863673e4e87f9bf2e
magiobus <magio@magiobus.com>
2025-01-05 16:34:42 -0600

    Better Siderbar

--- app/(private)/admin/dashboard/layout.js
+++ app/(private)/admin/dashboard/layout.js
@@ -2,22 +2,7 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 import config from "@/config";
-import Link from "next/link";
-import { HomeIcon, UsersIcon } from "@heroicons/react/24/outline";
-
-// Admin sidebar navigation options
-const sidebarOptions = [
-  {
-    href: "/admin/dashboard",
-    label: "Dashboard",
-    icon: HomeIcon,
-  },
-  {
-    href: "/admin/dashboard/users",
-    label: "Users",
-    icon: UsersIcon,
-  },
-];
+import Sidebar from "./Sidebar";
 
 // This is a server-side component to ensure the user is logged in and is an admin
 // If not, it will redirect to the login page.
@@ -48,20 +33,7 @@ export default async function LayoutAdminPrivate({ children }) {
           {children}
         </div>
       </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
-          {/* Sidebar content here */}
-          {sidebarOptions.map((option) => (
-            <li key={option.href}>
-              <Link href={option.href}>
-                <option.icon className="w-5 h-5" />
-                {option.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Sidebar />
     </div>
   );
 }
